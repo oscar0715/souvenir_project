9d14a11f48fb171d0dbb8873b1614487d51ae114
倪嘉铭 <oscarnjm@gmail.com>
2017-02-04 16:20:00 -0500

    template fit to mobile

--- souvenir/static/base_js/navbar.js
+++ souvenir/static/base_js/navbar.js
@@ -10,3 +10,11 @@
 		  );
 	}); // End Document Ready
 })(jQuery); // End of jQuery name space
+
+
+$('.button-collapse').sideNav({
+		menuWidth: 150, // Default is 300
+		closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+		draggable: true // Choose whether you can drag to open on touch screens
+	}
+);
