52dbe2059f3ca100dcbec75fd1e20a817857d039
倪嘉铭 <oscarnjm@gmail.com>
2017-03-20 15:42:44 -0400

    create new virtual environment

---
+++ env/new-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/oscar/Documents/souvenir_project/env/new-env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- env/souvenir_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/oscar/Documents/souvenir_project/env/souvenir_env/bin/python3.5
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
